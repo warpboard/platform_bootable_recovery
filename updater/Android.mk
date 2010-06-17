70d2db7c7b4b9ebc7d0bc005b8ad105a08560641
Hristo Bojinov <hristo@google.com>
2010-06-17 14:06:29 -0700

    Working ASLR implementation on korg/master.
    Separate files for retouch functionality in updater.
    ASLR for shared libraries is controlled by "-a" in ota_from_target_files.
    Retouch logic can recover from crashes, and uses the self-contained binary
    format that apriori outputs.
    
    Change-Id: I310a110667e9283b622566fd87a95a0cf6833fb7

--- updater/Android.mk
+++ updater/Android.mk
@@ -21,6 +21,7 @@ LOCAL_SRC_FILES := $(updater_src_files)
 LOCAL_STATIC_LIBRARIES := $(TARGET_RECOVERY_UPDATER_LIBS) $(TARGET_RECOVERY_UPDATER_EXTRA_LIBS)
 LOCAL_STATIC_LIBRARIES += libapplypatch libedify libmtdutils libminzip libz
 LOCAL_STATIC_LIBRARIES += libmincrypt libbz
+LOCAL_STATIC_LIBRARIES += libminelf
 LOCAL_STATIC_LIBRARIES += libcutils libstdc++ libc
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/..
 
